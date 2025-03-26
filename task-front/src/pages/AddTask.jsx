37802420d029a9667eb8b15ad3b9839177f3507b
sebastianosalzillo <salzillosebastiano@gmail.com>
2025-03-26 18:24:06 +0100

    feat: milestone 5 completato

--- task-front/src/pages/AddTask.jsx
+++ task-front/src/pages/AddTask.jsx
@@ -1,12 +1,75 @@
+import { useState, useRef } from "react"
+import { useGlobalContext } from "../context/GlobalContext"
+
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/`~"
+
 function AddTask() {
+  const [title, setTitle] = useState("")
+  const [error, setError] = useState("")
+  const descriptionRef = useRef()
+  const statusRef = useRef()
 
-    const apiUrl = import.meta.env.VITE_API_URL
+  const { addTask } = useGlobalContext()
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
+   
+    if (title.trim() === "") {
+      setError("Il nome del task è obbligatorio.")
+      return
+    }
 
+    if ([...title].some(char => symbols.includes(char))) {
+      setError("Il nome non può contenere simboli speciali.")
+      return
+    }
 
-    return (
-    <h2>Aggiungi un nuovo Task</h2>
-)
+    setError("")
+
+    const task = {
+      title,
+      description: descriptionRef.current.value,
+      status: statusRef.current.value
+    }
+
+    console.log("📝 Task da creare:", task)
+
+    
   }
-  export default AddTask
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>➕ Aggiungi Task</h2>
+
+      <div>
+        <label>Nome:</label><br />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <div>
+        <label>Descrizione:</label><br />
+        <textarea ref={descriptionRef}></textarea>
+      </div>
+
+      <div>
+        <label>Stato:</label><br />
+        <select defaultValue="To do" ref={statusRef}>
+          <option>To do</option>
+          <option>Doing</option>
+          <option>Done</option>
+        </select>
+      </div>
+
+      <button type="submit">Aggiungi Task</button>
+    </form>
+  )
+}
+
+export default AddTask
